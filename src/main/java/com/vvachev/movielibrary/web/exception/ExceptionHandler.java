0bd423f5f1bee619faa7d50cb9d4f5e621ef54ab
Viliqn Vachev <vvachev@axway.com>
2021-12-08 20:05:12 +0200

    Add custom error controller

--- src/main/java/com/vvachev/movielibrary/web/exception/ExceptionHandler.java
+++ src/main/java/com/vvachev/movielibrary/web/exception/ExceptionHandler.java
@@ -1,22 +1,29 @@
 package com.vvachev.movielibrary.web.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
-public class ExceptionHandler {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExceptionHandler implements ErrorController {
 	private static final String NOT_FOUND = "Page not found!";
 
-	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-	public ModelAndView handleErrors(Exception e, RuntimeException ex) {
-		ModelAndView modelAndView = new ModelAndView("error");
-		if (!e.getMessage().isEmpty()) {
-			modelAndView.addObject("errorMessage", e.getMessage());
+	@RequestMapping("/error")
+	public String handleError(Model model, HttpServletRequest request) {
+
+		String errorMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE).toString();
+		int status = Integer.valueOf(request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE).toString());
+
+		if (status == HttpStatus.NOT_FOUND.value()) {
+			model.addAttribute("errorMessage", NOT_FOUND);
 		} else {
-			modelAndView.addObject("errorMessage", NOT_FOUND);
+			model.addAttribute("errorMessage", errorMessage);
 		}
-		modelAndView.setStatus(HttpStatus.NOT_FOUND);
-		return modelAndView;
+		return "error";
 	}
 }
